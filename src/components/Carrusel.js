95959e448e4358f6d373b4d53fd7ee16f1f45d36
Daniel Vasquez <danielvasquez@Mac-mini-de-Daniel.local>
2021-05-18 00:04:29 -0500

    Agregando CSS Grid a la pantalla de Home

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -4,22 +4,47 @@ class Carrusel extends React.Component {
   constructor(props) {
     super();
 
-    this.state = {};
+    this.state = {
+      loading: true,
+      error: null,
+      data: undefined,
+    };
   }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    this.setState({ loading: true, error: null });
+
+    try {
+      const data = await fetch("https://rickandmortyapi.com/api/character/");
+      const response = await data.json();
+
+      console.log(response.results);
+      this.setState({ loading: false, data: response.results });
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
+  };
 
   render() {
     return (
       <section className="carousel">
         <div className="carousel__container">
-          <div className="carousel-item">
-            <img
-              className="carousel-item__img"
-              src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
-              alt=""
-            />
-            <p className="carousel-item__description">Hola</p>
-          </div>
-
+          {/* {this.state.data.map((data) => {
+            return (
+              <div className="carousel-item">
+                <img
+                  className="carousel-item__img"
+                  src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
+                  alt=""
+                />
+                <p className="carousel-item__description">{data.name}</p>
+              </div>
+            );
+          })} */}
           <div className="carousel-item">
             <img
               className="carousel-item__img"
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Carrusel from "../components/Carrusel.js";
+import "../components/styles/Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Soy Home</h1>
-      <Carrusel />
+    <div className="Home">
+      <div className="Home-container">
+        <Carrusel />
+        <h1>Soy componente chiquito</h1>
+      </div>
     </div>
   );
 };
